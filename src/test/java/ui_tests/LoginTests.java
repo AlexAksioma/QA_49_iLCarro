6c24582a630348b4fdce91c5d2822e2c3aa6bcf7
AlexAksioma <alexmedqwerty@gmail.com>
2025-10-27 19:27:48 +0200

    create reg negative tests

--- src/test/java/ui_tests/LoginTests.java
+++ src/test/java/ui_tests/LoginTests.java
@@ -31,4 +31,28 @@ public class LoginTests extends ApplicationManager {
         loginPage.typeLoginForm(user);
         Assert.assertTrue(loginPage.isLoggedIncorrect());
     }
+
+    @Test
+    public void loginNegativeTest_emptyPassword(){
+        User user = User.builder()
+                .username("bilbo_baggins_12345@mail.com")
+                .password("")
+                .build();
+        new HomePage(getDriver()).clickBtnLoginHeader();
+        LoginPage loginPage = new LoginPage(getDriver());
+        loginPage.typeLoginForm(user);
+        Assert.assertTrue(loginPage.isTextInErrorPresent("Password is required"));
+    }
+
+    @Test
+    public void loginNegativeTest_emailWOAt(){
+        User user = User.builder()
+                .username("bilbo_baggins_12345mail.com")
+                .password("Password123!")
+                .build();
+        new HomePage(getDriver()).clickBtnLoginHeader();
+        LoginPage loginPage = new LoginPage(getDriver());
+        loginPage.typeLoginForm(user);
+        Assert.assertTrue(loginPage.isTextInErrorPresent("It'snot look like email"));
+    }
 }
